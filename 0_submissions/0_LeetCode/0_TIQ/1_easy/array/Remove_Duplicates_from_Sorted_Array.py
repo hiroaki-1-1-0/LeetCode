4c025f58d871176d9d02bc49cddbc14e45f13e3e
hiroaki-1-1-0 <contact.hiroaki.110@gmail.com>
2025-01-01 23:58:17 +0900

    solve: TIQ easy 1

---
+++ 0_submissions/0_LeetCode/0_TIQ/1_easy/array/Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        cntr = 0
+        tmp = -1000
+        for num in nums:
+            if num != tmp:
+                nums[cntr] = num
+                cntr += 1
+            tmp = num
+        return cntr
